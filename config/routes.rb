0476bca40bc57c451e4cb62d79ae869cc082d422
シロー <kishi.rabbit313@gmail.com>
2021-06-15 13:49:12 +0900

    session view routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,15 @@ Rails.application.routes.draw do
   get 'topics/new'
   get 'topics/index'
   get 'sessions/new'
+  #ログインページへのroutes付け
+  get '/login', to: 'sesions#new'
+  get '/login', to: 'sesions#create'
+  get '/login', to: 'sesions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#index'
 
   resources :users
+
+
+
 end
