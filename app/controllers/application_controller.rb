360eba783f7b57064f0dd95fc7cfa90eb30fa17d
シロー <kishi.rabbit313@gmail.com>
2021-06-17 19:36:03 +0900

    login? gamenseni

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
 
+#sessionのuser_idからユーザ情報の取得
+  def current_user
+    @current_user ||= User.find_by(id:session[:user_id])
+  end
+#ユーザー情報が取得できているか確認
+  def logged_in?
+    !current_user.nil?
+  end
 end
