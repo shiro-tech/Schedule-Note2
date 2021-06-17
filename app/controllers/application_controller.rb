4975e9244d164f0b25f138d4651ae4c77b6aa77b
シロー <kishi.rabbit313@gmail.com>
2021-06-17 19:42:25 +0900

    helper syouryaku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
-
+  helper_method :current_user, :logged_in?
 #sessionのuser_idからユーザ情報の取得
   def current_user
     @current_user ||= User.find_by(id:session[:user_id])
